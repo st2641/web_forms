47857e41ba9e18ba0cc5e7ddd1d80fd72cebe8a5
capnkirk <shaun.cell74@gmail.com>
2015-05-14 21:59:20 -0400

    Created repo for web forms I am creating for church

---
+++ js/reg_inputs.js
@@ -0,0 +1,6 @@
+
+function processForm() {
+    var x = document.getElementById("reunion_registration").elements.length;
+    document.getElementById("demo").innerHTML = "Found " + x + " elements in the form.";
+}
+//var registrationForm = document.getElementById ('reunion_registration');
