32595e2ba12136ed7ff1a24b4e8228b9a997c5d2
capnkirk <shaun.cell74@gmail.com>
2015-06-06 08:59:04 -0400

    Cleaned up JS file and fixed how the functions were being called.

--- ReunionReg2015/public_html/js/reg_inputs.js
+++ ReunionReg2015/public_html/js/reg_inputs.js
@@ -1,44 +1,10 @@
-//Check to make sure that all required field are filled out
-//ref http://www.quirksmode.org/dom/error.html
-
-//var streetAddress        = document.getElementsByName.textFieldsReq[];
-//var cityCode             = document.getElementsByName.textFieldsReq[];
-//var stateCode            = document.getElementsByName.textFieldsReq[];
-//var zipCode              = document.getElementsByName.textFieldsReq[];
-//var phoneNumber          = document.getElementsByName.textFieldsReq[];
-//var emailAddress         = document.getElementsByName.textFieldsReq[];
-//var emergencyContactName = document.getElementsByName.textFieldsReq[];
-//var emergencyContactPhone= document.getElementsByName.textFieldsReq[];   
-//var mondayCheck          = document.getElementsByName.textFieldsReq[];
-//var tuesdayCheck         = document.getElementsByName.textFieldsReq[];
-//var wednesdayCheck       = document.getElementsByName.textFieldsReq[];
-//var thursdayCheck        = document.getElementsByName.textFieldsReq[];
-//var fridayCheck          = document.getElementsByName.textFieldsReq[];
-//var saturdayCheck        = document.getElementsByName.textFieldsReq[];
-//var sundayCheck          = document.getElementsByName.textFieldsReq[];
-//var beforeNoonMonYes     = document.getElementsByName.textFieldsReq[]; 
-//var beforeNoonMonNo      = document.getElementsByName.textFieldsReq[]; 
-//var eatMealsReunion      = document.getElementsByName.textFieldsReq[];
-//var noMealsReunion       = document.getElementsByName.textFieldsReq[];
-//var birLodge             = document.getElementsByName.textFieldsReq[];
-//var wilLodge             = document.getElementsByName.textFieldsReq[];
-//var camTrailRv           = document.getElementsByName.textFieldsReq[];
-//var tent                 = document.getElementsByName.textFieldsReq[];
-//var offGround            = document.getElementsByName.textFieldsReq[];
-//var fullName             = document.getElementsByName.textFieldsReq[];
-//Global Variables
 var checkboxes = document.getElementsByName('days');
-var valid      = false;
 
-
-
-
-
-function textFieldValid(){
+function textFieldValid() {
     var textFieldsReq = document.getElementsByName('textFieldReq');
-    for( var i=0;i<9;i++ ){
-        if ( !textFieldsReq[i].value ){
-            alert ( 'You need to fill in the required* text field!' );
+    for (var i = 0; i < textFieldsReq.length; i++) {
+        if (!textFieldsReq[i].value) {
+            alert('You need to fill in the required* text field!');
             textFieldsReq[i].focus();
             return false;
         }
@@ -46,42 +12,48 @@ function textFieldValid(){
     //if valid return true
     return true;
 };
-   
 
-function checkboxesValid(){
-    for ( var i = 0;i<checkboxes.length;i++ ){
-        if ( checkboxes[i].checked ) {
-         valid = true;
-         break;
-        }
-    }
-    if ( !valid ) {
-        alert( 'You need to select at least one day!' );
-        checkboxes[0].focus();
-        return false;
-    }
-    return valid;
-};
 
-function lodgeValid(){
-    var lodging = document.getElementsByName('lodge');
-    for( var i=0; i<lodging.length; i++ ){
-        if( lodging[i].checked ){
-            valid=true;
+function checkboxesValid() {
+    //create local variables
+    var valid = false;
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            valid = true;
             break;
         }
     }
-    if ( !valid ) {
-        alert( 'You need to select at least one option!' );
+    if (!valid) {
+        alert('You need to select at least one day!');
+        checkboxes[0].focus();
+        return false;
+    }
+    //if valid return true
+    return valid;
+}
+
+function lodgeValid() {
+    var lodging = document.getElementsByName('lodge'),
+        valid = false;
+    for (var i = 0; i < lodging.length; i++) {
+        if (lodging[i].checked) {
+            valid = true;
+            break;
+        }
+    }
+    if (!valid) {
+        alert('You need to select at least one option!');
         lodging[0].focus();
         return false;
     }
+    //if valid return true
     return valid;
-};
+}
 
 
-function validate(textFieldsReq){
-    return textFieldValid() && checkboxesValid() && lodgeValid();    
+function validate(textFieldsReq) {
+    //check whether all the tests are turnig true
+    return textFieldValid() && checkboxesValid() && lodgeValid();
 };
 
 function selectAll( form ){
